a13d1cc0b3a3fc65c465ec857c773df684990369
Abhishek Mahajan <abhishekm_c20022@students.isquareit.edu.in>
2024-01-07 13:30:19 +0530

    day-7 problem-3

---
+++ count_no_incremovable.cpp
@@ -0,0 +1,65 @@
+/*
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& n) {
+
+        set<vector<int>> s;
+        for(int i=0;i<n.size();i++){
+            vector<int>temp;
+
+            for(int j=i;j<n.size();j++){
+                temp.push_back(n[j]);
+
+                
+                if(temp.size()==1){
+                    s.insert(temp);
+                } else if(temp.size()>1){
+                    for(int k=1;k<temp.size();k++){
+                if(temp[k-1]<temp[k]){
+                    s.insert(temp);
+                    
+                }else if(temp[k-1]>=temp[k]){
+                    break;
+                }
+
+
+                }
+
+                }
+            }
+        }
+        return s.size();
+        
+    }
+
+};
+*/
+
+class Solution {
+public:
+    bool check(vector<int> tmp){
+        for(int i=1;i<tmp.size();i++){
+            if(tmp[i] <= tmp[i-1]) return false;
+        }
+        return true;
+    }
+    int incremovableSubarrayCount(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                vector<int> tmp;
+                for(int k=0;k<i;k++){
+                    tmp.push_back(nums[k]);
+                }
+                for(int k=j+1;k<n;k++){
+                   tmp.push_back(nums[k]);
+             }
+                if(check(tmp)){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
